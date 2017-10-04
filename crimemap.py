14c1038c7726d27f0d0f1a54e4ff309ccacadb7a
MMedia <knokle@live.dk>
2017-10-05 00:25:55 +0200

    blah bug tried fix

--- crimemap.py
+++ crimemap.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+
 app = Flask(__name__)
 DB = DBHelper()
+
 
 @app.route("/")
 def home():
@@ -16,14 +18,22 @@ def home():
     return render_template("home.html", data=data)
 
 
+@app.route("/add", methods=["POST"])
+def add():
+  try:
+    data = request.form.get("userinput")
+    DB.add_input(data)
+  except Exception as e:
+    print e
+  return home()
+
 @app.route("/clear")
 def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print e
-    return home()
-
-
+  try:
+    DB.clear_all()
+  except Exception as e:
+    print e
+  return home()
+  
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+  app.run(port=5000, debug=True)
